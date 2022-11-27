95b2e7278de36e6ffdcc49ca4e1ef70f17f2f8c7
Brian Dorsey <brian@dorseys.org>
2022-11-26 20:48:55 -0800

    day06 part 2

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -1,29 +1,13 @@
 use anyhow::Result;
-//use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::env;
-use std::fmt;
+//use std::fmt;
 //use std::fmt::Debug;
 use std::fs;
 use std::io::{prelude::*, BufReader};
 use tracing::{debug, info, Level};
 use tracing_subscriber::FmtSubscriber;
 //use std::ops;
-
-struct State(Vec<i32>);
-
-impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        let mut first = true;
-        for elem in &self.0 {
-            if !first {
-                write!(f, ",")?;
-            }
-            write!(f, "{}", elem)?;
-            first = false;
-        }
-        Ok(())
-    }
-}
 
 fn main() -> Result<()> {
     let subscriber = FmtSubscriber::builder()
@@ -44,30 +28,20 @@ fn main() -> Result<()> {
     let initial = lines.next().expect("failed to read first line")?;
     let initial: Vec<i32> = initial.split(',').map(|e| e.parse().unwrap()).collect();
 
-    let mut state = State(initial);
-    println!("Initial state: {}", state);
-    for _ in 1..=80 {
-        advance(&mut state);
-        //println!( "After {:>2} day{}: {}", i, if i == 1 { " " } else { "s" }, state);
+    let mut deque = VecDeque::from([0i64; 9]);
+    for e in &initial {
+        deque[*e as usize] += 1;
+    }
+    //dbg!(&deque);
+
+    for _ in 1..=256 {
+        deque.rotate_left(1);
+        deque[6] += deque[8];
+        //println!("{:?}", deque);
     }
 
-    println!("total fish: {}", state.0.len());
+    println!("total fish: {}", deque.iter().sum::<i64>());
     Ok(())
-}
-
-fn advance(state: &mut State) {
-    let mut new_fish = 0;
-    for elem in state.0.iter_mut() {
-        *elem -= 1;
-        // new fish!
-        if *elem < 0 {
-            *elem = 6;
-            new_fish += 1;
-        }
-    }
-    for _ in 0..new_fish {
-        state.0.push(8);
-    }
 }
 
 #[cfg(test)]
