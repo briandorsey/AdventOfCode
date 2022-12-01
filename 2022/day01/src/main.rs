526f5430b56987863068885e12120683a70206bc
Brian Dorsey <brian@dorseys.org>
2022-11-30 22:08:12 -0800

    day01

---
+++ 2022/day01/src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> anyhow::Result<()> {
+    println!("Hello, world!");
+
+    let filepath = env::args().nth(1).expect("please pass a file path");
+    println!("{filepath}");
+    let file = File::open(filepath)?;
+    let buf = io::BufReader::new(file);
+
+    let mut elves = vec![0];
+
+    for line in buf.lines() {
+        let line = line?;
+        if line.is_empty() {
+            elves.push(0);
+        } else {
+            *elves.last_mut().unwrap() += str::parse::<u32>(&line)?;
+        }
+        //println!("{line:?}");
+    }
+
+    elves.sort();
+    println!("day01p1: {:?}", &elves[elves.len() - 1]);
+    println!(
+        "day01p2: {:?}",
+        &elves[elves.len() - 3..elves.len()].iter().sum::<u32>()
+    );
+
+    Ok(())
+}
