ba1c4a3a3062c0cfe575f9a0400642cbb8880533
Brian Dorsey <brian@dorseys.org>
2022-12-01 21:04:32 -0800

    cleanups after reading various other folks solutions

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,31 +1,23 @@
-use std::env;
-use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() -> anyhow::Result<()> {
-    let filepath = env::args().nth(1).expect("please pass a file path");
-    println!("parsing: {filepath}");
-    let file = File::open(filepath)?;
-    let buf = io::BufReader::new(file);
-
     let mut elves = vec![0];
 
-    for line in buf.lines() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
         let line = line?;
         if line.is_empty() {
             elves.push(0);
         } else {
-            *elves.last_mut().unwrap() += str::parse::<u32>(&line)?;
+            *elves.last_mut().unwrap() += &line.parse::<u32>()?;
         }
         //println!("{line:?}");
     }
 
     elves.sort();
-    println!("day01p1: {:?}", &elves[elves.len() - 1]);
-    println!(
-        "day01p2: {:?}",
-        &elves[elves.len() - 3..elves.len()].iter().sum::<u32>()
-    );
+    elves.reverse();
+    println!("day01p1: {:?}", &elves.first().unwrap());
+    println!("day01p2: {:?}", &elves.iter().take(3).sum::<u32>());
 
     Ok(())
 }
