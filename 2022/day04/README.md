58717699b8d3380f6deb2889d5f204168a7cd263
Brian Dorsey <brian@dorseys.org>
2022-12-03 23:04:50 -0800

    day04 part 2

--- 2022/day04/README.md
+++ 2022/day04/README.md
@@ -1,9 +1,11 @@
 
+I tried to do this one as a chain of functions and only comparing ends of the ranges... and... I'm not happy with what I ended up with. :) 
+
 # confusion
-* 
+* I got somewhat lost in the math for finding overlaps. My initial working solution was actually missing a case that I only noticed while adding comments later... and then realized that case was actually covered by previous logic. Case in point. Seems like using std::ops::Range itself and/or HashSets or something where the compilier could help with the logic would be better. Will keep an eye out for that when looking at other's solutions
 
 # TIL
-* 
+* `.inspect()` is neat. 
 
 # review and iterate
 * 
--- 2022/day04/src/main.rs
+++ 2022/day04/src/main.rs
@@ -1,20 +1,22 @@
-//use anyhow::bail;
 use std::env;
 use std::fs;
-//use std::ops::Range;
+
+// this was an attempt to solve this one completely in one chain of functions... and it
+// technically works, but I'm not happy with the readability of this solution.
 
 fn main() -> anyhow::Result<()> {
     let input = env::args_os().nth(1).expect("need input file name");
     println!("{input:?}");
     let input = fs::read_to_string(input)?;
 
-    let count = input
+    // part 1
+    let contains = input
         .lines()
         .map(|e| e.split(',').collect::<Vec<_>>())
         .map(|mut v| {
             v.iter_mut()
                 .map(|e| {
-                    e.split("-")
+                    e.split('-')
                         .map(|i| i.parse::<i32>().unwrap())
                         .collect::<Vec<_>>()
                 })
@@ -25,23 +27,44 @@ fn main() -> anyhow::Result<()> {
             let l = &e[0];
             let r = &e[1];
             {
-                if (&l[0] <= &r[0] && &l[1] >= &r[1]) || (&r[0] <= &l[0] && &r[1] >= &l[1]) {
-                    true
-                } else {
-                    false
-                }
+                (l[0] <= r[0] && l[1] >= r[1]) || (r[0] <= l[0] && r[1] >= l[1])
             }
         })
         .filter(|e| *e)
         .count();
-    println!("{count:?}");
+    println!("contains: {contains:?}");
 
-    //println!("{:?}", Range { start: 3, end: 5 });
-    //for _i in (Range { start: 3, end: 8 }) {
-    //println!("{i}");
-    //}
-
-    //println!("{:?}", (1..5).contains(&9));
+    // part 2
+    let overlaps = input
+        .lines()
+        .map(|e| e.split(',').collect::<Vec<_>>())
+        .map(|mut v| {
+            v.iter_mut()
+                .map(|e| {
+                    e.split('-')
+                        .map(|i| i.parse::<i32>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .map(|e| {
+            //print!("{e:?}");
+            let l = &e[0];
+            let r = &e[1];
+            {
+                // 0th point of l inside r
+                (l[0] >= r[0] && l[0] <= r[1])
+                    // 1st point of l inside r
+                    || (l[1] >= r[0] && l[1] <= r[1])
+                    // l overlaps r
+                    || (l[0] <= r[0] && l[1] >= r[1])
+                // r overlaps l covered by the first two
+            }
+        })
+        //.inspect(|e| println!(" --> {e}"))
+        .filter(|e| *e)
+        .count();
+    println!("overlaps: {overlaps:?}");
 
     Ok(())
 }
