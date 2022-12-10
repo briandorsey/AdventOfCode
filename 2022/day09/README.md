6e8d25d77403eb22a1b51b5c1b3031321df1e146
Brian Dorsey <brian@dorseys.org>
2022-12-10 14:47:28 -0800

    day09 part 2

--- 2022/day09/README.md
+++ 2022/day09/README.md
@@ -1,9 +1,19 @@
+Continuing the trend, this one also is taking a very long time. Eventually got a part 1 working, and then the initial refactor for part 2 went surprisingly smoothly. But, I must be doing something slightly differently than the spec asks for, since it doesn't pass. Time for more logging and testing.  
+
+Finally, finnally got it working. In the end it turned out to be a logic error from reading the diagonal section of the spec wrong. 
+
+Did a lot of tracing and refactoring... so at least that practice is good for something? 
+
 
 ## confusion / questions
-* 
+* still confused by string concatenation sometimes. Ended up using `something.push_str(&something_else)`. Is that idomatic?
+* `usize` to `char`.... answer seems to be `char::from_digit(data as u32, 10)` ... really? ungh. 
+* all of the self.segs[i] indexing is a mess, must be a better way. When I tried to use slices, I ran into ownership/borrow issues.
+
 
 ## TIL
-* 
+* adding asserts to verify the exit state of movement was really helpful in development. Easy to see exactly where many logic errors happen.
+* Reading is hard. Always read the actual words of the spec. "the tail always moves one step diagonally to keep up". 
 
 ## review and iterate
 * has a [solution]()
--- 2022/day09/src/main.rs
+++ 2022/day09/src/main.rs
@@ -1,16 +1,32 @@
 use color_eyre::eyre::Result;
-//use color_eyre::eyre::{eyre, Result};
 use std::collections::HashSet;
 use std::env;
-use std::fmt;
+use std::fmt::{self, Write};
 use std::fs;
 use std::iter;
 use std::ops::Deref;
 use std::ops::DerefMut;
+use tracing::{debug, info, trace, Level};
+use tracing_subscriber::FmtSubscriber;
 
 fn main() -> Result<()> {
     let input = env::args_os().nth(1).expect("need input file name");
-    println!("{input:?}");
+    let level = match env::args().nth(2) {
+        Some(e) => match e.as_str() {
+            "debug" => Level::DEBUG,
+            "trace" => Level::TRACE,
+            _ => Level::INFO,
+        },
+        None => Level::INFO,
+    };
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(level)
+        .without_time()
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting tracing default subscriber failed");
+    info!("{input:?}");
     let input = fs::read_to_string(input)?;
 
     let moves: Vec<_> = input
@@ -19,51 +35,50 @@ fn main() -> Result<()> {
         .map(|(l, r)| (l, r.parse::<i32>().expect("parse to i32")))
         .collect();
 
-    let mut head = Position(0, 0);
-    let mut tail = Position(0, 0);
+    // hard coding these is a hack: segments, width, height
+    let grid_size = (6, 5);
+    let mut rope = Rope::new(10, grid_size);
     let mut history = History::new();
 
     for mov in moves {
-        print!("H:{head} T:{tail} | {mov:?} --> ");
-        match mov {
-            ("R", steps) => {
-                println!("[R:{}] --> ", steps);
-                for _ in 0..steps {
-                    head.0 += 1;
-                    update_tail(&head, &mut tail, &mut history);
-                }
-            }
-            ("L", steps) => {
-                println!("[L:{}] --> ", steps);
-                for _ in 0..steps {
-                    head.0 -= 1;
-                    update_tail(&head, &mut tail, &mut history);
-                }
-            }
-            ("U", steps) => {
-                println!("[U:{}] --> ", steps);
-                for _ in 0..steps {
-                    head.1 += 1;
-                    update_tail(&head, &mut tail, &mut history);
-                }
-            }
-            ("D", steps) => {
-                println!("[D:{}] --> ", steps);
-                for _ in 0..steps {
-                    head.1 -= 1;
-                    update_tail(&head, &mut tail, &mut history);
-                }
-            }
-            (&_, _) => unreachable!(),
-        }
-        println!("  --> H:{head} T:{tail}");
+        debug!("H:{} T:{} | {mov:?} --> ", rope.segs[0], rope.segs[1]);
+        history.extend(process_move(mov, &mut rope));
+        debug!("  --> H:{} T:{}", rope.segs[0], rope.segs[1]);
+        //rope._plot();
     }
 
-    //println!("part1: {}", history);
-    //history.plot();
-    println!("part1: {}", history.iter().count());
+    //history._plot(grid_size);
+    println!("part2: {}", history.iter().count());
 
     Ok(())
+}
+
+fn process_move(mov: (&str, i32), rope: &mut Rope) -> Vec<Position> {
+    let mut history: Vec<Position> = Vec::new();
+    let (dir, steps) = mov;
+    for _ in 0..steps {
+        match dir {
+            "R" => {
+                trace!("[R:{}] --> ", steps);
+                rope.segs[0].0 += 1;
+            }
+            "L" => {
+                trace!("[L:{}] --> ", steps);
+                rope.segs[0].0 -= 1;
+            }
+            "U" => {
+                trace!("[U:{}] --> ", steps);
+                rope.segs[0].1 += 1;
+            }
+            "D" => {
+                trace!("[D:{}] --> ", steps);
+                rope.segs[0].1 -= 1;
+            }
+            _ => unreachable!(),
+        };
+        history.push(rope.update_tail());
+    }
+    history
 }
 
 struct History {
@@ -77,25 +92,20 @@ impl History {
         }
     }
 
-    fn plot(&self) {
-        if let (Some(width), Some(height)) = (
-            self.set.iter().map(|e| e.0).max(),
-            self.set.iter().map(|e| e.1).max(),
-        ) {
-            //println!("{width}, {height}");
-            let mut grid: Vec<Vec<char>> = Vec::new();
-            for _ in 0..=height {
-                grid.push(iter::repeat('.').take(width as usize + 1).collect());
-            }
-            //println!("{}, {}", grid[0].len(), grid.len());
-            grid[0][0] = 's';
-            for pos in &self.set {
-                //println!("{pos}");
-                grid[pos.1 as usize][pos.0 as usize] = '#';
-            }
-            for row in grid.iter().rev() {
-                println!("{}", row.iter().collect::<String>());
-            }
+    fn _plot(&self, (width, height): (usize, usize)) {
+        trace!("{width}, {height}");
+        let mut grid: Vec<Vec<char>> = Vec::new();
+        for _ in 0..=height {
+            grid.push(iter::repeat('.').take(width as usize + 1).collect());
+        }
+        trace!("{}, {}", grid[0].len(), grid.len());
+        grid[0][0] = 's';
+        for pos in &self.set {
+            //println!("{pos}");
+            grid[pos.1 as usize][pos.0 as usize] = '#';
+        }
+        for row in grid.iter().rev() {
+            debug!("{}", row.iter().collect::<String>());
         }
     }
 }
@@ -123,6 +133,136 @@ impl DerefMut for History {
 }
 
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
+struct Rope {
+    segs: Vec<Position>,
+    width: usize,
+    height: usize,
+}
+
+impl Rope {
+    fn new(count: usize, (width, height): (usize, usize)) -> Rope {
+        Rope {
+            segs: iter::repeat(Position(0, 0)).take(count).collect(),
+            width,
+            height,
+        }
+    }
+
+    fn _plot(&self) -> Result<String> {
+        fn pretty(i: usize) -> char {
+            match i {
+                0 => 'H',
+                i => char::from_digit(i as u32, 10).unwrap_or('?'),
+            }
+        }
+        let mut grid: Vec<Vec<char>> = Vec::new();
+        for _ in 0..self.height {
+            grid.push(iter::repeat('.').take(self.width as usize).collect());
+        }
+        grid[0][0] = 's';
+        for (i, pos) in self.segs.iter().enumerate().rev() {
+            //println!("{pos}");
+            grid[pos.1 as usize][pos.0 as usize] = pretty(i);
+        }
+        let mut output = String::new();
+        for row in grid.iter().rev() {
+            writeln!(output, "{}", row.iter().collect::<String>())?;
+        }
+        Ok(output)
+    }
+
+    fn update_tail(&mut self) -> Position {
+        // wanted to iterate on .windows() here, but it wasn't available initialy and isn't mutable
+        // anyway. Again, I'm probably structuring this wrong. Should this kind of function take
+        // ownership and return at the end instead of a mutable borrow?
+        // Also... all of this self.segs[i] indexing is a mess, must be a better way.
+
+        for i in 0..self.segs.len() - 1 {
+            let head = i;
+            let tail = i + 1;
+            let mut note = format!(
+                "  ut:{head} H:{} T:{} --> ",
+                self.segs[head], self.segs[tail]
+            );
+
+            // case: equal
+            if self.segs[head] == self.segs[tail] {
+                continue;
+            }
+
+            // case: vertical
+            if self.segs[head].1 == self.segs[tail].1 {
+                let diff = self.segs[head].0 - self.segs[tail].0;
+                if diff >= 1 {
+                    note.push_str("move right --> ");
+                    self.segs[tail].0 += diff.abs() - 1;
+                } else if diff <= -1 {
+                    note.push_str("move left --> ");
+                    self.segs[tail].0 -= diff.abs() - 1;
+                };
+            } else if self.segs[head].0 == self.segs[tail].0 {
+                let diff = self.segs[head].1 - self.segs[tail].1;
+                if diff >= 1 {
+                    note.push_str("move up --> ");
+                    self.segs[tail].1 += diff.abs() - 1;
+                } else if diff <= -1 {
+                    note.push_str("move down --> ");
+                    self.segs[tail].1 -= diff.abs() - 1;
+                };
+            }
+
+            // case: diagonal
+            if (self.segs[head].0 != self.segs[tail].0) && (self.segs[head].1 != self.segs[tail].1)
+            {
+                let diffx = self.segs[head].0 - self.segs[tail].0;
+                let diffy = self.segs[head].1 - self.segs[tail].1;
+                if diffx.abs() >= 2 || diffy.abs() >= 2 {
+                    if diffx > 0 && diffy > 0 {
+                        note.push_str("diag up & right --> ");
+                        self.segs[tail].0 += 1;
+                        self.segs[tail].1 += 1;
+                    } else if diffx < 0 && diffy > 0 {
+                        note.push_str("diag up & left --> ");
+                        self.segs[tail].0 -= 1;
+                        self.segs[tail].1 += 1;
+                    } else if diffx > 0 && diffy < 0 {
+                        note.push_str("diag down & right --> ");
+                        self.segs[tail].0 += 1;
+                        self.segs[tail].1 -= 1;
+                    } else if diffx < 0 && diffy < 0 {
+                        note.push_str("diag down & left --> ");
+                        self.segs[tail].0 -= 1;
+                        self.segs[tail].1 -= 1;
+                    } else {
+                        //println!("H:{}, T:{}", self.segs[head], self.segs[tail]);
+                        note.push_str("diag no-op--> ");
+                    }
+                }
+            }
+
+            note.push_str(&format!("T:{}", self.segs[tail]));
+            trace!("{}", note);
+
+            // double check: tail should never be more than one step behind
+            assert!(
+                (self.segs[head].0 - self.segs[tail].0).abs() <= 1,
+                "update_tail check: H:{} T:{}",
+                self.segs[head],
+                self.segs[tail]
+            );
+            assert!(
+                (self.segs[head].1 - self.segs[tail].1).abs() <= 1,
+                "update_tail check: H:{} T:{}",
+                self.segs[head],
+                self.segs[tail]
+            );
+        }
+        debug!("\n{}", self._plot().unwrap());
+        self.segs[self.segs.len() - 1].clone()
+    }
+}
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 struct Position(i32, i32);
 
 impl fmt::Display for Position {
@@ -132,61 +272,33 @@ impl fmt::Display for Position {
     }
 }
 
-fn update_tail_inline(head: &Position, tail: &mut Position) {
-    // case: directly in-line from each other
-    if head.1 == tail.1 {
-        if head.0 - tail.0 >= 1 {
-            //print!("move right --> ");
-            tail.0 += 1;
-        } else if head.0 - tail.0 <= 1 {
-            //print!("move left --> ");
-            tail.0 -= 1;
-        };
-    } else if head.0 == tail.0 {
-        if head.1 - tail.1 >= 1 {
-            //print!("move up --> ");
-            tail.1 += 1;
-        } else if head.1 - tail.1 <= 1 {
-            //print!("move down --> ");
-            tail.1 -= 1;
-        };
-    }
-}
-fn update_tail(head: &Position, tail: &mut Position, history: &mut History) {
-    //print!("  ut: H:{head} T:{tail} --> ");
-
-    // case: diagonal
-    if !(head.0 == tail.0 && head.1 == tail.1) {
-        if (head.0 - tail.0).abs() >= 2 {
-            //print!("align vertical --> ");
-            tail.1 = head.1;
-            update_tail_inline(head, tail);
-        } else if (head.1 - tail.1).abs() >= 2 {
-            //print!("align horizontal --> ");
-            tail.0 = head.0;
-            update_tail_inline(head, tail);
-        }
-    }
-
-    //println!("{tail}");
-    // double check: tail should never be more than one step behind
-    assert!(
-        !((head.0 - tail.0).abs() > 1),
-        "update_tail check: H:{head} T:{tail}",
-    );
-    assert!(
-        !((head.1 - tail.1).abs() > 1),
-        "update_tail check: H:{head} T:{tail}",
-    );
-    history.insert(tail.clone());
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_true() {
-        assert!(true);
+    fn test_certain_long_tail_cases() {
+        let grid_size = (6, 5);
+        let mut rope = Rope::new(10, grid_size);
+        process_move(("R", 4), &mut rope);
+        let expected = r#"
+......
+......
+......
+......
+4321H.
+"#;
+        assert_eq!(expected.trim(), rope._plot().unwrap().trim());
+
+        process_move(("U", 4), &mut rope);
+        let expected = r#"
+....H.
+....1.
+..432.
+.5....
+6.....
+"#;
+        println!("{}\n{}", expected.trim(), rope._plot().unwrap().trim());
+        assert_eq!(expected.trim(), rope._plot().unwrap().trim());
     }
 }
