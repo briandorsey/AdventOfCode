a6bb6bc32d90dc611f62798a942a6652de641ba7
Brian Dorsey <brian@dorseys.org>
2022-12-13 21:26:13 -0800

    day13 part 2

--- 2022/day13/README.md
+++ 2022/day13/README.md
@@ -1,6 +1,9 @@
+Got lost in nested match statements. Don't do logic too late at night. Also, did the logic manually... while this is *probably* a sort problem that could be solved by implmenting comparison / sorting? 
+
+Implemented Ord & Eq traits to allow sorting for part2... seems like there might be a cleaner way to do it, but I just delegated to the funciton I'd created before. 
 
 ## confusion / questions
-* 
+*
 
 ## TIL (Learned... or Looked up)
 * 
--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -1,40 +1,55 @@
 use color_eyre::eyre::Result;
 use itertools::{EitherOrBoth, Itertools};
 use serde::Deserialize;
+use std::cmp::Ordering;
 use std::env;
 use std::fmt::Display;
 use std::fs;
+use tracing::{debug, info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 fn main() -> Result<()> {
     let input = env::args_os().nth(1).expect("need input file name");
-    println!("{input:?}");
+    let level = match env::args().nth(2) {
+        Some(e) => match e.as_str() {
+            "debug" => Level::DEBUG,
+            "trace" => Level::TRACE,
+            _ => Level::INFO,
+        },
+        None => Level::INFO,
+    };
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(level)
+        .without_time()
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting tracing default subscriber failed");
+    info!("{input:?}");
     let input = fs::read_to_string(input)?;
 
     let packets: Vec<_> = input.lines().filter(|l| !l.trim().is_empty()).collect();
-    let packets: Vec<_> = packets.chunks(2).map(|p| (p[0], p[1])).collect();
-    let packets: Vec<(_, _)> = packets
+    let mut packets: Vec<_> = packets
         .iter()
-        .map(|(l, r)| {
-            (
-                Item::Items(serde_json::from_str(l).expect("json")),
-                Item::Items(serde_json::from_str(r).expect("json")),
-            )
-        })
+        .map(|p| Item::Items(serde_json::from_str(p).expect("json")))
+        .collect();
+    let packet_pairs: Vec<_> = packets
+        .chunks(2)
+        .map(|p| (p[0].clone(), p[1].clone()))
         .collect();
 
-    //println!("{:?}", is_orderd(&packets[0]));
-    for (i, packet) in packets.iter().enumerate() {
-        println!("== Pair {} == ", i + 1);
+    for (i, packet) in packet_pairs.iter().enumerate() {
+        debug!("== Pair {} == ", i + 1);
 
-        //println!("- Compare {} vs. {}", packet.0, packet.1);
+        //debug!("- Compare {} vs. {}", packet.0, packet.1);
         let outcome = is_orderd(&packet.0, &packet.1, 1).unwrap();
-        println!("{outcome}");
+        debug!("{outcome}");
 
-        println!();
+        debug!("");
     }
-    println!("==============");
+    debug!("==============");
 
-    let part1: usize = packets
+    let part1: usize = packet_pairs
         .iter()
         .map(|p| is_orderd(&p.0, &p.1, 1).unwrap())
         .enumerate()
@@ -42,35 +57,49 @@ fn main() -> Result<()> {
         .filter(|(_, v)| *v)
         .map(|(i, _)| i)
         .sum();
-    dbg!(part1);
+    info!("part 1: {}", part1);
 
+    let div_a = Item::Items(vec![Item::Items(vec![Item::Int(2)])]);
+    let div_b = Item::Items(vec![Item::Items(vec![Item::Int(6)])]);
+    packets.push(div_a.clone());
+    packets.push(div_b.clone());
+    packets.sort();
+    let mut total = 1;
+    for (i, p) in packets.iter().enumerate() {
+        println!("{i:>3}: {p}");
+        if p == &div_a || p == &div_b {
+            total *= i + 1;
+            println!("  ^--div");
+        }
+    }
+    info!("part 2: {}", total);
     Ok(())
 }
 
 fn is_orderd(left: &Item, right: &Item, ind: usize) -> Option<bool> {
     match (left, right) {
         (Item::Int(l), Item::Int(r)) if l == r => {
-            println!("{:>ind$} Compare {} vs {}", "-", l, r);
+            debug!("{:>ind$} Compare {} vs {}", "-", l, r);
             None
         }
         (Item::Int(l), Item::Int(r)) if l < r => {
-            println!("{:>ind$} Compare {} vs {}", "-", l, r);
-            println!(
+            debug!("{:>ind$} Compare {} vs {}", "-", l, r);
+            debug!(
                 "  {:>ind$} Left side is smaller, so inputs are in the right order",
                 "-"
             );
             Some(true)
         }
         (Item::Int(l), Item::Int(r)) if l > r => {
-            println!("{:>ind$} Compare {} vs {}", "-", l, r);
-            println!(
+            debug!("{:>ind$} Compare {} vs {}", "-", l, r);
+            debug!(
                 "  {:>ind$} Right side is smaller, so inputs are not in the right order",
                 "-"
             );
             Some(false)
         }
         (Item::Items(l), Item::Items(r)) => {
-            println!(
+            debug!(
                 "{:>ind$} Compare [{}] vs [{}]",
                 "-",
                 l.iter().join(","),
@@ -79,15 +108,15 @@ fn is_orderd(left: &Item, right: &Item, ind: usize) -> Option<bool> {
             for pair in l.iter().zip_longest(r.iter()) {
                 let option = match pair {
                     EitherOrBoth::Both(l, r) => {
-                        //println!("{:>ind$} Compare {} vs {}", "-", l, r);
+                        //debug!("{:>ind$} Compare {} vs {}", "-", l, r);
                         is_orderd(l, r, ind + 2)
                     }
                     EitherOrBoth::Right(_) => {
-                        println!("{:>ind$} Left side ran out of items, so inputs are not in the right order", "-");
+                        debug!("{:>ind$} Left side ran out of items, so inputs are not in the right order", "-");
                         Some(true)
                     }
                     EitherOrBoth::Left(_) => {
-                        println!("{:>ind$} Right side ran out of items, so inputs are not in the right order", "-");
+                        debug!("{:>ind$} Right side ran out of items, so inputs are not in the right order", "-");
                         Some(false)
                     }
                 };
@@ -99,14 +128,14 @@ fn is_orderd(left: &Item, right: &Item, ind: usize) -> Option<bool> {
             None
         }
         (Item::Int(l), Item::Items(_)) => {
-            println!(
+            debug!(
                 "{:>ind$} Mixed types; convert left to [{}] and retry comparison",
                 "-", l
             );
             is_orderd(&Item::Items(vec![Item::Int(*l)]), right, ind + 2)
         }
         (Item::Items(_), Item::Int(r)) => {
-            println!(
+            debug!(
                 "{:>ind$} Mixed types; convert right to [{}] and retry comparison",
                 "-", r
             );
@@ -122,6 +151,39 @@ enum Item {
     Int(i32),
     Items(Vec<Item>),
 }
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> std::option::Option<std::cmp::Ordering> {
+        if self == other {
+            return Some(Ordering::Equal);
+        };
+        if is_orderd(self, other, 1).expect("is_order() failed!") {
+            Some(Ordering::Less)
+        } else {
+            Some(Ordering::Greater)
+        }
+    }
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).expect("partial_cmp() failed!")
+    }
+}
+
+impl PartialEq for Item {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Item::Int(s), Item::Int(o)) => s == o,
+            (Item::Int(_), Item::Items(_)) => false,
+            (Item::Items(_), Item::Int(_)) => false,
+            // is the below valid? I think so because eventually we get ints
+            (Item::Items(s), Item::Items(o)) => s == o,
+        }
+    }
+}
+
+impl Eq for Item {}
 
 impl Display for Item {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
