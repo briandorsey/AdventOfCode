b12eb5284ec5e0755a2599ab7a84c83d9d20cae2
Brian Dorsey <brian@dorseys.org>
2022-12-13 18:58:01 -0800

    day13 part 1

--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -1,10 +1,9 @@
 use color_eyre::eyre::Result;
-//use color_eyre::eyre::{eyre, Result};
+use itertools::{EitherOrBoth, Itertools};
 use serde::Deserialize;
 use std::env;
 use std::fmt::Display;
 use std::fs;
-use std::ops::Deref;
 
 fn main() -> Result<()> {
     let input = env::args_os().nth(1).expect("need input file name");
@@ -28,16 +27,16 @@ fn main() -> Result<()> {
         println!("== Pair {} == ", i + 1);
 
         //println!("- Compare {} vs. {}", packet.0, packet.1);
-        let outcome = is_orderd(packet, 1);
+        let outcome = is_orderd(&packet.0, &packet.1, 1).unwrap();
         println!("{outcome}");
 
-        println!("");
+        println!();
     }
     println!("==============");
 
     let part1: usize = packets
         .iter()
-        .map(|p| is_orderd(&p, 1))
+        .map(|p| is_orderd(&p.0, &p.1, 1).unwrap())
         .enumerate()
         .map(|(i, v)| (i + 1, v))
         .filter(|(_, v)| *v)
@@ -45,86 +44,75 @@ fn main() -> Result<()> {
         .sum();
     dbg!(part1);
 
-    // 1048 low
-
     Ok(())
 }
 
-fn is_orderd((left, right): &(Item, Item), ind: usize) -> bool {
-    /*
-    println!("{:>ind$} Compare {} vs {}", "-", left, right);
-    if left.len() == 0 {
-        println!(
-            "{:>ind$} Left side ran out of items, so inputs are in the right order",
-            "-"
-        );
-        true
-    } else if right.len() == 0 {
-        println!(
-            "{:>ind$} Right side ran out of items, so inputs are not in the right order",
-            "-"
-        );
-        false
-    } else {
-    */
+fn is_orderd(left: &Item, right: &Item, ind: usize) -> Option<bool> {
     match (left, right) {
-        // todo rework logic to work from Items directly
-        // there is actually no way to continue from here in this architecture... we need to move
-        // onward, but we don't have the list anymore. initial logic needs to have a loop.
         (Item::Int(l), Item::Int(r)) if l == r => {
             println!("{:>ind$} Compare {} vs {}", "-", l, r);
-            is_orderd(
-                &(
-                    Packet::new(left[1..].to_vec()),
-                    Packet::new(right[1..].to_vec()),
-                ),
-                ind + 2,
-            )
+            None
         }
-        (Item::Int(l), Item::Int(r)) => {
+        (Item::Int(l), Item::Int(r)) if l < r => {
             println!("{:>ind$} Compare {} vs {}", "-", l, r);
-            l < r
+            println!(
+                "  {:>ind$} Left side is smaller, so inputs are in the right order",
+                "-"
+            );
+            Some(true)
+        }
+        (Item::Int(l), Item::Int(r)) if l > r => {
+            println!("{:>ind$} Compare {} vs {}", "-", l, r);
+            println!(
+                "  {:>ind$} Right side is smaller, so inputs are not in the right order",
+                "-"
+            );
+            Some(false)
         }
         (Item::Items(l), Item::Items(r)) => {
-            if !is_orderd(&(Packet::new(l.to_vec()), Packet::new(r.to_vec())), ind + 2) {
-                return false;
+            println!(
+                "{:>ind$} Compare [{}] vs [{}]",
+                "-",
+                l.iter().join(","),
+                r.iter().join(",")
+            );
+            for pair in l.iter().zip_longest(r.iter()) {
+                let option = match pair {
+                    EitherOrBoth::Both(l, r) => {
+                        //println!("{:>ind$} Compare {} vs {}", "-", l, r);
+                        is_orderd(l, r, ind + 2)
+                    }
+                    EitherOrBoth::Right(_) => {
+                        println!("{:>ind$} Left side ran out of items, so inputs are not in the right order", "-");
+                        Some(true)
+                    }
+                    EitherOrBoth::Left(_) => {
+                        println!("{:>ind$} Right side ran out of items, so inputs are not in the right order", "-");
+                        Some(false)
+                    }
+                };
+                if option.is_none() {
+                    continue;
+                };
+                return option;
             }
-
-            is_orderd(
-                &(
-                    Packet::new(left[1..].to_vec()),
-                    Packet::new(right[1..].to_vec()),
-                ),
-                ind + 2,
-            )
+            None
         }
-        (Item::Int(l), Item::Items(r)) => {
+        (Item::Int(l), Item::Items(_)) => {
             println!(
                 "{:>ind$} Mixed types; convert left to [{}] and retry comparison",
                 "-", l
             );
-            is_orderd(
-                &(
-                    Packet::new([Item::Int(*l)].to_vec()),
-                    Packet::new(r.to_vec()),
-                ),
-                ind + 2,
-            )
+            is_orderd(&Item::Items(vec![Item::Int(*l)]), right, ind + 2)
         }
-        (Item::Items(l), Item::Int(r)) => {
+        (Item::Items(_), Item::Int(r)) => {
             println!(
                 "{:>ind$} Mixed types; convert right to [{}] and retry comparison",
                 "-", r
             );
-
-            is_orderd(
-                &(
-                    Packet::new(l.to_vec()),
-                    Packet::new([Item::Int(*r)].to_vec()),
-                ),
-                ind + 2,
-            )
+            is_orderd(left, &Item::Items(vec![Item::Int(*r)]), ind + 2)
         }
+        (l, r) => todo!("{:?}, {:?}", l, r),
     }
 }
 
@@ -150,38 +138,6 @@ impl Display for Item {
                 )?;
             }
         }
-        Ok(())
-    }
-}
-
-struct Packet {
-    p: Vec<Item>,
-}
-
-impl Packet {
-    fn new(p: Vec<Item>) -> Packet {
-        Packet { p }
-    }
-}
-
-impl Deref for Packet {
-    type Target = Vec<Item>;
-    fn deref(&self) -> &<Self as Deref>::Target {
-        &self.p
-    }
-}
-
-impl Display for Packet {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "[{}]",
-            self.p
-                .iter()
-                .map(|i| i.to_string())
-                .collect::<Vec<_>>()
-                .join(",")
-        )?;
         Ok(())
     }
 }
