d73b15ca99bbd48e0743d3d1c8e2e90ee5e8aa93
Brian Dorsey <brian@dorseys.org>
2022-12-17 13:52:55 -0800

    initial template & inputs

---
+++ 2022/day17/README.md
@@ -0,0 +1,14 @@
+
+## confusion / questions
+* 
+
+## TIL (Learned... or Looked up)
+* 
+
+## review and iterate
+* has a [solution]()
+* 
+
+## things to learn about
+* 
+
---
+++ 2022/day17/src/main.rs
@@ -0,0 +1,22 @@
+use color_eyre::eyre::Result;
+//use color_eyre::eyre::{eyre, Result};
+use std::env;
+use std::fs;
+
+fn main() -> Result<()> {
+    let input = env::args_os().nth(1).expect("need input file name");
+    println!("{input:?}");
+    let _input = fs::read_to_string(input)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_true() {
+        assert!(true);
+    }
+}
