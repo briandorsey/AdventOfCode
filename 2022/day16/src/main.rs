92003a6de164bc4565d4cb6af748f64e53611f6d
Brian Dorsey <brian@dorseys.org>
2022-12-17 19:37:20 -0800

    non working attempt at day16 part 1 (petgraph + vec of paths)

--- 2022/day16/src/main.rs
+++ 2022/day16/src/main.rs
@@ -1,119 +1,199 @@
 use color_eyre::eyre::Result;
+use petgraph::graph::{Graph, NodeIndex};
 use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fmt::{Display, Formatter};
 use std::fs;
+use tracing::{debug, info, trace, Level};
+use tracing_subscriber::FmtSubscriber;
 
 fn main() -> Result<()> {
     let input = env::args_os().nth(1).expect("need input file name");
-    //println!("{input:?}");
+    let level = match env::args().nth(2) {
+        Some(e) => match e.as_str() {
+            "debug" => Level::DEBUG,
+            "trace" => Level::TRACE,
+            _ => Level::INFO,
+        },
+        None => Level::INFO,
+    };
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(level)
+        .without_time()
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting tracing default subscriber failed");
+    info!("{input:?}");
     let input = fs::read_to_string(input)?;
 
-    let mut valves = HashMap::<ValveID, Valve>::new();
+    let mut graph: Graph<Valve, i32, petgraph::Directed> = Graph::new();
+    let mut nodes: HashMap<ValveID, NodeIndex> = HashMap::new();
+    let mut edges: Vec<(ValveID, ValveID)> = Vec::new();
     for line in input.lines() {
         let line = line.replace("Valve ", "");
         let line = line.replace("tunnels", "tunnel");
         let line = line.replace("valves", "valve");
         let line = line.replace("leads", "lead");
         //println!("{line:?}");
-        let (id, line) = line.split_once(" has flow rate=").expect("parsing id");
+        let (valveid, line) = line.split_once(" has flow rate=").expect("parsing id");
         let (flow_rate, line) = line
             .split_once("; tunnel lead to valve ")
             .expect("parsing flow_rate");
-        let tunnels: Vec<_> = line.split(", ").map(|e| (1, e.to_string())).collect();
+
+        let tunnels: Vec<_> = line
+            .split(", ")
+            .map(|e| (valveid.to_string(), e.to_string()))
+            .collect();
+        edges.extend(tunnels);
+
         let valve = Valve {
-            id: id.to_string(),
+            id: valveid.to_string(),
             flow_rate: flow_rate.parse::<i32>().expect("flow_rate to int"),
-            tunnels: tunnels,
         };
+        let node = graph.add_node(valve);
+        nodes.insert(valveid.to_string(), node);
         //println!("  --> {valve}");
-        // diy HashMap graph
-        valves.insert(id.to_string(), valve.clone());
+    }
+    for (a, b) in edges {
+        //println!("{a:?} --> {b:?}");
+        graph.add_edge(nodes[&a], nodes[&b], 1);
     }
 
-    //let valves = _collapse_zeros(valves);
+    // graph info
+    //graph.remove_node(nodes["II"]);
+    trace!("graph.is_directed(): {}", graph.is_directed());
+    trace!("graph.node_count(): {}", graph.node_count());
+    trace!("graph.edge_count(): {}", graph.edge_count());
 
-    // lets make a dot file by hand
+    trace!("");
+    //println!("{}", petgraph::dot::Dot::new(&graph));
+    //println!("{:#?}", &graph);
+
+    // summarize nodes - remove 0 rate nodes with only two peers
     if false {
-        println!("digraph G {{\nAA [style=bold] \n");
-        for valve in valves.values() {
-            println!(
-                "{} [label=\"{} fr{}\"];",
-                valve.id, valve.id, valve.flow_rate
-            );
-            for t in &valve.tunnels {
-                println!("{} -> {} [label=\"s{}\"];", valve.id, t.1, t.0);
+        graph.retain_nodes(|g, n| {
+            trace!("{:?}", n);
+            let valve = &g[n];
+            if valve.flow_rate == 0 && g.neighbors(n).collect::<Vec<_>>().len() == 2 {
+                trace!("  {:?}", valve);
+                for edge in g.edges_directed(n, petgraph::Outgoing) {
+                    trace!("    O: {:?}", edge);
+                }
+                for edge in g.edges_directed(n, petgraph::Incoming) {
+                    trace!("    I: {:?}", edge);
+                }
+                for neighbor in g.neighbors(n) {
+                    trace!("    {:?}", g[neighbor]);
+                }
             }
-        }
-        println!("}}");
+            true
+        });
     }
 
-    //candidate paths - for now, let's just try to fill out all possible options.
-    if false {
-        let max_iterations = 3;
-        // let mut paths: Vec<Action> = Vec::new();
-        let mut positions = vec![String::from("AA")];
-        for i in 1..=max_iterations {
-            let tmp: Vec<ValveID> = Vec::new();
-            for pos in positions.drain(..) {
-                println!("{i}: {} {:?}", valves[&pos], valves[&pos].tunnels);
-                //tmp.extend(valves[&pos].tunnels.clone());
+    //let mut paths: Graph<Action, (), petgraph::Directed> = Graph::new();
+    //paths.add_node(Action::Start(String::from("AA")));
+    let mut paths: Vec<Path> = Vec::new();
+    paths.push(Path {
+        actions: vec![Action::Start(String::from("AA"))],
+        open: HashSet::new(),
+        visited: HashSet::new(),
+    });
+    //let mut positions: Vec<NodeIndex> = Vec::new();
+    //positions.push(nodes["AA"]);
+    let node_count = graph.node_count();
+    let mut completed_paths: Vec<Path> = Vec::new();
+
+    for iteration in 1..=25 {
+        debug!("== Minute {iteration} ==");
+        let mut next_paths: Vec<Path> = Vec::new();
+
+        for path in &mut paths {
+            if path.visited.len() == node_count {
+                completed_paths.push(path.clone());
+                continue;
             }
-            //println!("positions.len(): {}", positions.len());
-            positions = tmp;
+            trace!("-- {path}");
+            let i = path.actions.len() - 1;
+            match &mut path.actions[i] {
+                Action::MoveTo(id)
+                    if &graph[nodes[id]].flow_rate > &0 && !path.open.contains(id) =>
+                {
+                    let valve = &graph[nodes[id]];
+                    trace!("---- open valve! {valve}");
+                    let new_path = &mut path.clone();
+                    new_path.actions.push(Action::Open(
+                        valve.id.to_string(),
+                        graph[nodes[&valve.id]].flow_rate,
+                    ));
+                    next_paths.push(new_path.clone());
+                    continue;
+                }
+                Action::Start(id) | Action::Open(id, _) | Action::MoveTo(id) => {
+                    let valve = &graph[nodes[id]];
+                    trace!("---- {valve}");
+                    for neighbor in graph.neighbors(nodes[id]) {
+                        trace!("  \\-->{:?}", &graph[neighbor].id);
+                        let new_path = &mut path.clone();
+                        new_path.visited.insert(valve.id.to_string());
+                        new_path
+                            .actions
+                            .push(Action::MoveTo(graph[neighbor].id.to_string()));
+                        next_paths.push(new_path.clone());
+                    }
+                }
+            }
         }
+        paths = next_paths;
+        debug!(
+            "   paths: {}    complete: {}",
+            paths.len(),
+            completed_paths.len()
+        );
     }
 
     Ok(())
 }
 
-// ok, giving up now. I somehow forgot that most nodes have bidirectional links
-fn _collapse_zeros(mut valves: HashMap<String, Valve>) -> HashMap<String, Valve> {
-    println!("collapse_zeros()");
-    let mut found = 0;
-    let reference = valves.clone();
-    let mut output = HashMap::new();
-    let mut orphans: Vec<ValveID> = Vec::new();
-    for (k, mut v) in valves.drain() {
-        println!("{}", v);
-        if v.tunnels.len() == 1 {
-            println!("tunnels.len() = 1 {}", v);
-            for t in v.tunnels.iter_mut() {
-                println!("{:?} fr: {}", t, reference[&t.1].flow_rate,);
-                if reference[&t.1].flow_rate == 0 {
-                    println!("{:?}", t);
-                    orphans.push(t.1.clone());
-                    found += 1;
-                    t.0 += reference[&t.1].tunnels[0].0;
-                    t.1 = reference[&t.1].tunnels[0].1.clone();
-                }
-            }
-        }
-        output.insert(k, v);
-    }
-    //for o in orphans {
-    //output.remove(&o);
-    //}
-
-    println!("-- end collapse_zeros()");
-    if found > 0 {
-        _collapse_zeros(output)
-    } else {
-        output
-    }
-}
-
 //fn parse_input(input: &str) ->
 
-enum _Action {
+#[derive(Debug, Clone)]
+enum Action {
+    Start(ValveID),
     MoveTo(ValveID),
     Open(ValveID, FlowRate),
 }
+impl Display for Action {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Action::Start(id) => write!(f, "S({id})"),
+            Action::MoveTo(id) => write!(f, "M({id})"),
+            Action::Open(id, fr) => write!(f, "O({id}, {fr})"),
+        }
+    }
+}
 
-struct _Path {
-    actions: Vec<_Action>,
+#[derive(Debug, Clone)]
+struct Path {
+    actions: Vec<Action>,
     open: HashSet<ValveID>,
-    flow_rate: FlowRate,
+    visited: HashSet<ValveID>,
+    //flow_rate: FlowRate,
+}
+
+impl Display for Path {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "P:({}):{:?}",
+            self.actions
+                .iter()
+                .map(|a| format!("{}", a))
+                .collect::<Vec<_>>()
+                .join(","),
+            self.open,
+        )
+    }
 }
 
 type ValveID = String;
@@ -125,26 +205,15 @@ enum State {
     _Closed,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Valve {
     id: ValveID,
     flow_rate: FlowRate,
-    tunnels: Vec<(u32, ValveID)>,
 }
 
 impl Display for Valve {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "{}:{}->{}",
-            self.id,
-            self.flow_rate,
-            self.tunnels
-                .iter()
-                .map(|t| { format!("{}s{}", t.0, t.1) })
-                .collect::<Vec<_>>()
-                .join(",")
-        )
+        write!(f, "{}:{}", self.id, self.flow_rate,)
     }
 }
 
