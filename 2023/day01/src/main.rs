4829735cbd6c49206157736ccb34eea21a43d18e
Brian Dorsey <brian@dorseys.org>
2023-12-07 22:25:49 -0800

    day01 part02 WIP - probably never finish on principle

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -2,6 +2,7 @@ use color_eyre::eyre::Result;
 //use color_eyre::eyre::{eyre, Result};
 use std::env;
 //use std::fmt::Display;
+use std::collections::HashMap;
 use std::fs;
 
 fn main() -> Result<()> {
@@ -9,20 +10,56 @@ fn main() -> Result<()> {
     println!("{input:?}");
     let _input = fs::read_to_string(input)?;
 
+    let mut r: HashMap<&str, &str> = HashMap::new();
+    r.insert("one", "1");
+    r.insert("two", "2");
+    r.insert("three", "3");
+    r.insert("four", "4");
+    r.insert("five", "5");
+    r.insert("six", "6");
+    r.insert("seven", "7");
+    r.insert("eight", "8");
+    r.insert("nine", "9");
+    print!("{:?}\n", r);
+
     let mut values = Vec::new();
     for line in _input.lines() {
         print!("{}\n", line);
-        let nums: Vec<_> = line.chars().filter_map(|c| c.to_digit(10)).collect();
-        print!("{:?}\n", nums);
-        print!("first: {:?}, last: {:?}\n", nums.first(), nums.last());
-        if let (Some(first), Some(last)) = (nums.first(), nums.last()) {
-            values.push(first * 10 + last);
+        if let (Some(left), Some(right)) = (left(line, &r), right(line, &r)) {
+            values.push(left * 10 + right);
         }
     }
     print!("{:?}\n", values);
     print!("output:\n{:?}\n", values.iter().sum::<u32>());
 
     Ok(())
+}
+
+fn left(s: &str, r: &HashMap<&str, &str>) -> Option<u32> {
+    let nums: Vec<_> = s.chars().filter_map(|c| c.to_digit(10)).collect();
+    print!("{:?} -> ", nums);
+    print!("first: {:?}\n", nums.first());
+    nums.first().copied()
+}
+
+fn right(s: &str, r: &HashMap<&str, &str>) -> Option<u32> {
+    let nums: Vec<_> = s.chars().filter_map(|c| c.to_digit(10)).collect();
+    print!("{:?} -> ", nums);
+    print!("last: {:?}\n", nums.last());
+    nums.last().copied()
+}
+
+fn _preprocess(s: &str) -> String {
+    let mut out = s.replace("one", "1");
+    out = out.replace("two", "2");
+    out = out.replace("three", "3");
+    out = out.replace("four", "4");
+    out = out.replace("five", "5");
+    out = out.replace("six", "6");
+    out = out.replace("seven", "7");
+    out = out.replace("eight", "8");
+    out = out.replace("nine", "9");
+    out
 }
 
 #[cfg(test)]
